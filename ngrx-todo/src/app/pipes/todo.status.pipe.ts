3015310d765c00387dd872d57b8a56af6e2e18b5
Maarten Merken <merken.maarten@gmail.com>
2018-02-22 14:22:42 +0100

    TSlint fix

--- ngrx-todo/src/app/pipes/todo.status.pipe.ts
+++ ngrx-todo/src/app/pipes/todo.status.pipe.ts
@@ -5,8 +5,9 @@ import { Todo } from '../models';
 @Pipe({ name: 'todoStatus' })
 export class TodoStatusPipe implements PipeTransform {
   transform(todos: Todo[], status: string) {
-    if(todos === null)
+    if (todos === null) {
       return [];
+    }
     return todos.filter(todo => todo.status === status);
   }
 }
