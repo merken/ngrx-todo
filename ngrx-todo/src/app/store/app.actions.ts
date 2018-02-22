292208b73e3bf4317dd26a62fdb426533ba19b0f
Maarten Merken <merken.maarten@gmail.com>
2018-02-22 15:11:38 +0100

    more setup

--- ngrx-todo/src/app/store/app.actions.ts
+++ ngrx-todo/src/app/store/app.actions.ts
@@ -5,14 +5,3 @@ export const CONNECTION_ESTABLISHED = '[App] Connection Established';
 export const SUBSCRIPTION_COMPLETED = '[App] Subscription Completed';
 export const REQUEST_DISPATCHED = '[App] Request Dispatched';
 
-export const LOAD_TODOS = '[App] Load Todos';
-export const TODOS_LOADED = '[App] Todos Loaded';
-
-export const ADD_TODO = '[App] Add Todo';
-export const TODO_ADDED = '[App] Todo Added';
-
-export const UPDATE_TODO = '[App] Update Todo';
-export const TODO_UPDATED = '[App] Todo Updated';
-
-export const DELETE_TODO = '[App] Delete Todo';
-export const TODO_DELETED = '[App] Todo Deleted';
