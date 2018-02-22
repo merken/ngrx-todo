bd60f2fc2f2664b69fa3f3b5870a01f1e637d3ba
Maarten Merken <merken.maarten@gmail.com>
2018-02-22 15:14:25 +0100

    setup

--- ngrx-todo/src/app/store/app.actions.ts
+++ ngrx-todo/src/app/store/app.actions.ts
@@ -3,5 +3,5 @@ import { Todo } from '../models';
 
 export const CONNECTION_ESTABLISHED = '[App] Connection Established';
 export const SUBSCRIPTION_COMPLETED = '[App] Subscription Completed';
+export const MESSAGE_RECEIVED = '[App] Message Received';
 export const REQUEST_DISPATCHED = '[App] Request Dispatched';
-
